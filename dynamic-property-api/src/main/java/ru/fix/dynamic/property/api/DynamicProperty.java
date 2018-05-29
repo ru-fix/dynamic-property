00e36cb48fd114c49ea6aa50f9faaf7cb21c6894
kasfandiyarov <kasfandiyarov@gmail.com>
2018-05-29 12:18:11 +0300

    Add javadoc to property interface

--- dynamic-property-api/src/main/java/ru/fix/dynamic/property/api/DynamicProperty.java
+++ dynamic-property-api/src/main/java/ru/fix/dynamic/property/api/DynamicProperty.java
@@ -1,9 +1,22 @@
 package ru.fix.dynamic.property.api;
 
+/**
+ * Holds property value and notify listener when value is changed.
+ * Implementation should provide thread safe access to property value.
+ */
 public interface DynamicProperty<T> {
 
+    /**
+     * @return current value of property
+     */
     T get();
 
+    /**
+     * Add listener to dynamic property
+     *
+     * @param listener Listener runs whenever property value changes.
+     *                 It is implementation specific in what thread listener will be invoked.
+     */
     void addListener(DynamicPropertyListener<T> listener);
 
     static <T> DynamicProperty<T> of(T value) {
