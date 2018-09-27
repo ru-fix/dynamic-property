6c906bdc7d66567d9b81a3f107b2babb2f7299c1
Andrey <akiselev@jet.msk.su>
2018-09-27 09:05:40 +0300

    fix codacy issue

--- dynamic-property-api/src/main/java/ru/fix/dynamic/property/polling/PolledProperty.java
+++ dynamic-property-api/src/main/java/ru/fix/dynamic/property/polling/PolledProperty.java
@@ -5,27 +5,23 @@ package ru.fix.dynamic.property.polling;
  * @author Andrey Kiselev
  */
 
-import java.util.List;
 import java.util.Objects;
-import java.util.ArrayList;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import ru.fix.dynamic.property.api.DynamicProperty;
 import ru.fix.dynamic.property.api.AtomicProperty;
-import ru.fix.dynamic.property.api.DynamicPropertyListener;
 
-public class PolledProperty<PolledType> extends AtomicProperty<PolledType>
-    implements DynamicProperty<PolledType>  {
-    private final Supplier<PolledType> retriever;
+public class PolledProperty<T> extends AtomicProperty<T>
+    implements DynamicProperty<T>  {
+    private final Supplier<T> retriever;
 
-    public PolledProperty(Supplier<PolledType> retriever) {
+    public PolledProperty(Supplier<T> retriever) {
         super(retriever.get());
         this.retriever = retriever;
     }
 
     public void poll() {
-        PolledType pValue = retriever.get();
+        T pValue = retriever.get();
         if( ! Objects.equals(pValue, get())) {
             set(pValue);
         }
