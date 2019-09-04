a291dc299e2e8fffa6ab4375dc4f0f2e0699e922
kasfandiyarov <kasfandiyarov@gmail.com>
2019-09-04 16:39:04 +0300

    fix dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,6 +90,7 @@ subprojects {
         outputDirectory = "$buildDir/dokka"
 
         //TODO: wait dokka support JDK11 - https://github.com/Kotlin/dokka/issues/428
+        //TODO: wait dokka fix https://github.com/Kotlin/dokka/issues/464
         enabled = false
     }
 
