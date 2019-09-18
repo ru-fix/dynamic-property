a0a352a0665691185c7a02fb9f1428245eb7cadc
kasfandiyarov <kasfandiyarov@gmail.com>
2019-09-18 15:09:11 +0300

    return of(Supplier) api

---
+++ dynamic-property-api/src/main/java/ru/fix/dynamic/property/api/SuppliedProperty.java
@@ -0,0 +1,51 @@
+package ru.fix.dynamic.property.api;
+
+import java.util.function.Supplier;
+
+/**
+ * Be aware that SupplierProperty
+ * does not notify listeners through {@link java.beans.PropertyChangeListener}
+ * All requests to {@link DynamicProperty#get()}
+ * will be delegated to {@link Supplier#get()} method of given supplier.
+ *
+ * If you need a DynamicProperty with full listener support backed up by {@link Supplier} use DynamicPropertyPoller
+ * @see ru.fix.dynamic.property.polling.DynamicPropertyPoller
+ */
+public class SuppliedProperty<T> implements DynamicProperty<T> {
+
+    private final Supplier<T> supplier;
+
+    public SuppliedProperty(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    @Override
+    public T get() {
+        return supplier.get();
+    }
+
+    @Override
+    public DynamicProperty<T> addListener(DynamicPropertyListener<T> listener) {
+        return this;
+    }
+
+    @Override
+    public DynamicProperty<T> addAndCallListener(DynamicPropertyListener<T> listener) {
+        listener.onPropertyChanged(supplier.get());
+        return this;
+    }
+
+    @Override
+    public T addListenerAndGet(DynamicPropertyListener<T> listener) {
+        return supplier.get();
+    }
+
+    @Override
+    public DynamicProperty<T> removeListener(DynamicPropertyListener<T> listener) {
+        return this;
+    }
+
+    @Override
+    public void close() {
+    }
+}
