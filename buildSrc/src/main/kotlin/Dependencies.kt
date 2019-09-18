614e2b25d38fa2b077ad42bb598eaea6b4d243ec
kasfandiyarov <kasfandiyarov@gmail.com>
2019-09-18 15:10:46 +0300

    return of(Supplier) api

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -36,7 +36,7 @@ object Libs {
     const val mockk = "io.mockk:mockk:1.9.3"
     const val awaitility = "org.awaitility:awaitility:4.0.1"
 
-    val jfix_stdlib_concurrency = "ru.fix:jfix-stdlib-concurrency:1.0.54"
+    val jfix_stdlib_concurrency = "ru.fix:jfix-stdlib-concurrency:1.0.56"
     val aggregating_profiler = "ru.fix:aggregating-profiler:1.5.16"
 
     const val slf4j_api = "org.slf4j:slf4j-api:${Vers.sl4j}"
