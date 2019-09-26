50df62732192043d0003b6a47209d2a6b48f52b7
Azat Mukhametzyanov <amukhametzyanov@fix.ru>
2019-09-26 14:57:46 +0300

    add test for checking props not default

---
+++ dynamic-property-zk/src/test/kotlin/ru/fix/dynamic/property/zk/ZkDynamicPropertySourceIT.kt
@@ -0,0 +1,78 @@
+package ru.fix.dynamic.property.zk
+
+import org.awaitility.Awaitility
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import ru.fix.dynamic.property.jackson.JacksonDynamicPropertyMarshaller
+import ru.fix.dynamic.property.source.DefaultValue
+import ru.fix.zookeeper.testing.ZKTestingServer
+import java.nio.charset.StandardCharsets
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+
+class ZkDynamicPropertySourceIT {
+    companion object {
+        private const val PROPERTIES_LOCATION = "/zookeeper/p"
+        private val log = LoggerFactory.getLogger(ZkDynamicPropertySource::class.java)
+    }
+
+    private lateinit var zkTestingServer: ZKTestingServer
+    private lateinit var source: ZkDynamicPropertySource
+
+    @BeforeEach
+    fun beforeEach() {
+        zkTestingServer = ZKTestingServer().start()
+        source = ZkDynamicPropertySource(
+                zkTestingServer.client,
+                PROPERTIES_LOCATION,
+                JacksonDynamicPropertyMarshaller(),
+                Duration.of(1, ChronoUnit.MINUTES)
+        )
+    }
+
+    @AfterEach
+    fun afterEach() {
+        source.close()
+        zkTestingServer.close()
+    }
+
+    @Test
+    fun `when loading a large number of properties`() {
+        val generatedProperties = generateProperties(10000)
+
+        generatedProperties.forEach {
+            setServerProperty("$PROPERTIES_LOCATION/${it.key}", it.value)
+        }
+
+        generatedProperties.forEach {
+            source.subscribeAndCallListener(
+                    it.key,
+                    String::class.java,
+                    DefaultValue.of("default")
+            ) { value ->
+                Assertions.assertNotEquals("default", value)
+            }
+        }
+    }
+
+    private fun setServerProperty(propertyKey: String, value: String) {
+        val data = value.toByteArray(StandardCharsets.UTF_8)
+        zkTestingServer.client
+                .create()
+                .creatingParentsIfNeeded()
+                .forPath(propertyKey, data)
+    }
+
+    private fun generateProperties(count: Int): Map<String, String> {
+        val generatedProperties = HashMap<String, String>()
+
+        for (i in 0..count) {
+            generatedProperties["prop-$i"] = "value-$i"
+        }
+
+        return generatedProperties
+    }
+}
