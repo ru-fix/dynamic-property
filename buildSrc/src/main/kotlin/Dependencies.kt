ba3f0e520e54e95c71c1190eeb232571987eacb5
akazankov <akazankov@fix.ru>
2020-06-01 18:16:41 +0300

    aggregating_profiler upgrade 1.5.21 -> 1.6.5

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -17,7 +17,7 @@ object Vers {
     const val log4j = "2.12.0"
 
     const val jfix_stdlib = "3.0.2"
-    const val aggregating_profiler = "1.5.21"
+    const val aggregating_profiler = "1.6.5"
 }
 
 object Libs {
