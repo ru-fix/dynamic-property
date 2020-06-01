c595aad21b06b43fbd6c29a11c62a62118595c5f
akazankov <akazankov@fix.ru>
2020-06-01 14:02:53 +0300

    revert changes in buildSrc build script

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,10 +1,7 @@
+import org.gradle.kotlin.dsl.*
+
 plugins {
     `kotlin-dsl`
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 repositories {
