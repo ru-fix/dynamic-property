48344898975980d0ba51e2435a3227a496a4ba73
akazankov <akazankov@fix.ru>
2020-06-01 10:34:57 +0300

    downgrade java target version to 8 in api module

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,10 @@
-import org.gradle.kotlin.dsl.*
-
 plugins {
     `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 repositories {
