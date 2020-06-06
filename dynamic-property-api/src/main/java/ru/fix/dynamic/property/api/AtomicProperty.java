90c8482e803a339af7873a42c20eeed6f29e924c
kasfandiyarov <VkyabOvtTzp1821>
2020-06-06 14:58:00 +0300

    Update docs

--- dynamic-property-api/src/main/java/ru/fix/dynamic/property/api/AtomicProperty.java
+++ dynamic-property-api/src/main/java/ru/fix/dynamic/property/api/AtomicProperty.java
@@ -13,40 +13,18 @@ import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Provides simple implementation for DynamicProperty. <br>
- * Allows to manually provide DynamicProperty in tests. <br>
- * Be aware that this class does not synchronize value update and listener invocations.<br>
+ * Be aware that you can see new value update through {@link #get()}
+ * before receiving update event in {@link PropertyListener}
  * Listeners will be invoked during {@link #set(Object)} in the same thread. <br>
  * <br>
  * <pre>{@code
- * MyService(DynamicProperty<Integer> property){
- *      property.addAndCallListener(value -> initialize(value))
- * }
- * property = AtomicProperty(1)
+ * MyService(DynamicProperty<Integer> property){...}
+ *
+ * val property = AtomicProperty(1)
  * MyService myService = MyService(property)
  * result = myService.doWork()
- * assertThat(result, ...)
  * }</pre>
  * <p>
- * Be aware that listeners invoked in same thread, that calls {@link #set(Object)} <br>
- * This class does not provide any synchronization except holding volatile variable in order to stay lightweight <br>
- * Here is an example that leads to race condition:
- *
- * <pre>{@code
- * MyService(DynamicProperty<Integer> property){
- *      property.addAndCallListener(value -> initialize(value))
- * }
- * //DO NOT DO THAT
- * property = AtomicProperty(1)
- * new Thread{ property.set(2) }.start()
- * MyService myService = MyService(property)
- * //Problem 1. It is not clear what value MyService will see 1 or 2 or both.
- * //Problem 2. MyService can end up with a stale value of 1.
- * //Problem 3. MyService method initialize could be invoked twice concurrently.
- * //Problem 4. MyService method initialize could be invoked twice and see wrong order of changes:
- * // at first it will see 2 and then 1.
- * }</pre>
- * <p>
- * In order to prevent all four problems user of AtomicProperty should organize proper thread safe usages of the property.
  *
  * @author Kamil Asfandiyarov
  */
