e0d8ea7c88a1ed7f167caa22c0c68bc457575559
kasfandiyarov <VkyabOvtTzp1821>
2020-06-07 14:36:49 +0300

    Update docs for PropertyId

--- dynamic-property-api/src/main/java/ru/fix/dynamic/property/api/annotation/PropertyId.java
+++ dynamic-property-api/src/main/java/ru/fix/dynamic/property/api/annotation/PropertyId.java
@@ -5,7 +5,66 @@ import java.lang.annotation.*;
 
 /**
  * Unique identifies property within property source
+ * Used in automatic injection scenarios
+ * <pre>{@code
+ * class MyService{
+ *   @PropertyId("my.service.timeout)
+ *   DynamicProperty<Integer> timeout;
  *
+ *   @PropertyId("my.service.settings)
+ *   PropertySubscription<Settings> settings;
+ * }
+ * }</pre>
+ *
+ * You can provide default values by explicit field initialization:
+ * <pre>{@code
+ * class MyService{
+ *   @PropertyId("my.service.timeout)
+ *   DynamicProperty<Integer> timeout = DynamicProperty.of(120);
+ * }
+ * }</pre>
+ *
+ * After dependency injection phase completion you can subscribe to dynamic property change by listeners:
+ * <pre>{@code
+ * class MyService{
+ *   @PropertyId("my.service.timeout)
+ *   DynamicProperty<Integer> timeout = DynamicProperty.of(120);
+ *   PropertySubscription<Integer> timeoutSubscription;
+ *
+ *   @PostConstruct
+ *   void initialize(){
+ *     timeoutSubscription = timeout.createSubscription().setAndCallListener((oldValue, newValue)->{...})
+ *   }
+ * }
+ * }</pre>
+ *
+ * Or use simplified version with automatic subscription:
+ * <pre>{@code
+ * class MyService{
+ *   @PropertyId("my.service.timeout)
+ *   PropertySubscription<Integer> timeout = PropertySubscription.of(120);
+ * }
+ * }</pre>
+ *
+ * Be aware that dynamic property field will be replaced with new one during dependency injection process.
+ * Usually dependency injection framework inject dependencies after object construction.
+ * Do not use or subscribe to DynamicProperty instances that are temporal and works only as a default value provider
+ * for a dependency injection frameworks.
+ * <pre>{@code
+ * // DO NOT DO THAT!
+ * class MyService{
+ *   @PropertyId("my.service.timeout)
+ *   DynamicProperty<Integer> timeout = DynamicProperty.of(120);
+ *   PropertySubscription<Integer> timeoutSubscription;
+ *   MyService(){
+ *     //Constructor sees 'default' instance of DynamicProperty with value 120
+ *     //But this instance will be replaced with another one during dependency injection phase
+ *     //All subscription will be lost and listeners will no longer be invoked
+ *     timeoutSubscription = timeout.createSubscription()
+ *                      .setAndCallListener((oldValue, newValue)->{...})
+ *   }
+ * }
+ * }</pre>
  * @author Kamil Asfandiyarov
  */
 @Target(value = {ElementType.FIELD})
