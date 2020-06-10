8b320bc521acca18e4860d368ab9333d162f70bd
Владимир Малыгин <vmalygin@fix.ru>
2020-06-10 11:39:52 +0500

    remove unused dependency to aggregating-profiler

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -20,7 +20,6 @@ object Vers {
     const val log4j =  "2.12.0"
 
     const val jfix_stdlib = "2.0.16"
-    const val aggregating_profiler = "1.5.21"
 }
 
 object Libs {
@@ -42,7 +41,6 @@ object Libs {
     val jfix_stdlib_concurrency = "ru.fix:jfix-stdlib-concurrency:${Vers.jfix_stdlib}"
     val jfix_stdlib_reference = "ru.fix:jfix-stdlib-reference:${Vers.jfix_stdlib}"
     val jfix_stdlib_files = "ru.fix:jfix-stdlib-files:${Vers.jfix_stdlib}"
-    val aggregating_profiler = "ru.fix:aggregating-profiler:${Vers.aggregating_profiler}"
 
     const val slf4j_api = "org.slf4j:slf4j-api:${Vers.sl4j}"
     const val slf4j_simple = "org.slf4j:slf4j-simple:${Vers.sl4j}"
